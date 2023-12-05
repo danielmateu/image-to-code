a88857f2cb3d09c4a88eb46ff8dd415cf6940503
Daniel Mateu <danielmateu86@gmail.com>
2023-12-05 15:02:31 +0100

    conectando a api gpt4

---
+++ components/Form.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+
+export const Form = () => {
+
+    return (
+        <form
+            onSubmit={(e) => {
+                e.preventDefault()
+                const form = e.currentTarget as HTMLFormElement
+                const url = form.elements.namedItem('url') as HTMLInputElement
+                console.log(url.value);
+            }}
+            
+        >
+            <div className="flex flex-col gap-2 pb-4">
+                <Label htmlFor="url">Introduce tu url de la imagen:</Label>
+                <Input name="url" id="url" type="url" placeholder="https://..." />
+            </div>
+            <Button type="submit">Generar código</Button>
+        </form>
+    )
+}
